76f878a755fe256eec2b6d2b22d65068b814fa9e
sergeyrubinsteingit <sergey.rubinshtein@gmail.com>
2021-10-30 20:17:50 +0300

    Update TestCaseToExtentReport.java

--- src/test_program/program_files_/TestCaseToExtentReport.java
+++ src/test_program/program_files_/TestCaseToExtentReport.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -20,7 +21,7 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import test_program.Default_StartBrowser;
 
 
-public class TestCaseToExtentReport extends org.testng.TestListenerAdapter implements org.testng.IInvokedMethodListener
+public class TestCaseToExtentReport extends org.testng.TestListenerAdapter implements org.testng.IInvokedMethodListener, ITestListener 
  {
     //helps to generate the logs in extnTest report.
     public static ExtentTest  extnTest;    
@@ -28,7 +29,7 @@ public class TestCaseToExtentReport extends org.testng.TestListenerAdapter imple
     public static ITestResult testingResult;    
 	public static WebDriver webDriver = Default_StartBrowser.webDriver;
 	public static boolean vlinksFlg_TestCase = true;
-
+	
 	
     @Test(alwaysRun = true)
     public static void testingCase() throws IOException {
@@ -38,11 +39,11 @@ public class TestCaseToExtentReport extends org.testng.TestListenerAdapter imple
     	boolean theFlag = Start_STR.theFlag;
 
     // Creates a test entry in HTML report file [ExtentRepLog.html]
-    	extnTest = SetHTMLReporter.extnReports.createTest("Test Case Nmb " + VisitLinks.linksCounter, 
+    	extnTest = SetHTMLReporter.extnReports.createTest("Test Case Nmb " + (VisitLinks.linksCounter + 1), 
     			"::: PAGE'S TITLE: " + Start_STR.getPageData[0] + " :::");
     	webDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     // Adds a node to the test entry	
-    	extnTestNode = extnTest.createNode("A Node for Test Case Nmb " + VisitLinks.linksCounter, 
+    	extnTestNode = extnTest.createNode("A Node for Test Case Nmb " + (VisitLinks.linksCounter + 1), 
     			"::: PAGE'S URL: " + Start_STR.getPageData[1] + " :::");
     	webDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     	
@@ -56,7 +57,13 @@ public class TestCaseToExtentReport extends org.testng.TestListenerAdapter imple
     } // eomth testingCase
 
     @AfterMethod
-    public static  void getResult(ITestResult testingResult) throws IOException {
+    public static  void getResult(ITestResult testingResult_) throws IOException {
+    	
+    	testingResult = testingResult_;
+    	
+    	if (Start_STR.theFlag == false) {
+			TestCaseToExtentReport.testingResult.setStatus(ITestResult.FAILURE);
+		}//eoif
     	
     	System.out.println("############## The testingResult is " + testingResult.toString() + " ################");
     	
@@ -82,7 +89,7 @@ public class TestCaseToExtentReport extends org.testng.TestListenerAdapter imple
 			extnTest.log(Status.ERROR, trrr.getMessage());
 			System.err.println("Error in TestCase : getResult" + trrr.getMessage() + "\n=======================================");
 		}
-    }
+    }//eo getResult
 
 	@AfterClass
     public static void tearDown() throws IOException {
@@ -92,4 +99,4 @@ public class TestCaseToExtentReport extends org.testng.TestListenerAdapter imple
 		vlinksFlg_TestCase = true;
 		System.out.println("\n000000000000000   TearDown's running   000000000000000\n");
     }
-}
+}//eoteardown
